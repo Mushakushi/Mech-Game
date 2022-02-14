560345e93de53416533a9cd84c8e6b81f275fa39
ALEXANDER KRELL <xkrell10@gmail.com>
2022-02-14 13:40:50 -0500

    Sort Animators, Start Projectile System

--- Assets/Scripts/ScriptableObjects/AttackProjectileAsset.cs
+++ Assets/Scripts/ScriptableObjects/AttackProjectileAsset.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,29 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "[attack name]", menuName ="Create Attack...")]
 public class AttackProjectileAsset : ScriptableObject
 {
-    
+    public AnimatorOverrideController projectileAnimations;
+    public AttackDirections attackSpawnDirections;
+    public int damage;
+}
+
+[Serializable]
+public struct AttackDirections
+{
+    [SerializeField] public bool top;
+    [SerializeField] public bool sides;
+
+    public AttackDirections(bool top, bool sides)
+    {
+        this.top = top;
+        this.sides = sides;
+    }
+}
+
+public struct HitboxProperties
+{
+    [SerializeField] public float sizeX;
+    [SerializeField] public float sizeY;
+
+    [SerializeField] public float offsetX;
+    [SerializeField] public float offsetY;
 }
